63101d7be430b660552048aaf80cfb23544dbc0f
boomsik <pozdnyakov151994@gmail.com>
2024-08-03 21:22:30 +0300

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
 import React from "react";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./redux/store";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 
 const App = () => {
     return (
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <h1>Phonebook</h1>
-                <ContactForm />
-                <h2>Contacts</h2>
-                <SearchBox />
-                <ContactList />
-            </PersistGate>
-        </Provider>
+        <div>
+            <h1>Phonebook</h1>
+            <ContactForm />
+            <h2>Contacts</h2>
+            <SearchBox />
+            <ContactList />
+        </div>
     );
 };
 
