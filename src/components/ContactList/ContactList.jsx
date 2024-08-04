1b4c45e0a56b2d9481add3d4d65378ec12e84745
boomsik <pozdnyakov151994@gmail.com>
2024-08-04 13:07:41 +0300

    fix contact search

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
 import Contact from "../Contact/Contact";
 import styles from "./ContactList.module.css";
 
 const ContactList = () => {
     const contacts = useSelector(selectContacts);
+    const filter = useSelector(selectNameFilter);
+
+    const filteredContacts = contacts.filter((contact) =>
+        contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
     return (
         <ul className={styles.list}>
-            {contacts.map((contact) => (
+            {filteredContacts.map((contact) => (
                 <Contact key={contact.id} contact={contact} />
             ))}
         </ul>
